76be5d5e40b85e8e44b23cbb679af4725a6a58f9
mauro <maurobilardo02@gmail.com>
2025-09-09 16:44:27 +0200

    devo migliorare nel css

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ let listaSpesa = [];
 
 function aggiornaCarrello(){
     let lista = document.getElementById("listaSpesa");
-    lista.innerHTML = "";
+    lista.textContent = "";
     for (let oggetto of listaSpesa){
         let li = document.createElement("li");
         li.textContent = oggetto;
@@ -12,11 +12,15 @@ function aggiornaCarrello(){
 
 function aggiungiAlCarrello(){
     let elemento = document.getElementById("oggetto").value.trim();
-
     if (elemento !== ""){
         listaSpesa.push(elemento);
         elemento.value = "";
         aggiornaCarrello();
     }
 
+}
+
+function svuotaCarrello(){
+    listaSpesa.splice(0 , listaSpesa.length)
+    aggiornaCarrello();
 }
